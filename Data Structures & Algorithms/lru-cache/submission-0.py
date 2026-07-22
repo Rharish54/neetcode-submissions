63437980f3c25a5525d91331e0c94b3d5a615b7b
Rahul Harish <140627060+Rharish54@users.noreply.github.com>
2026-07-22 02:17:09 -0400

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,29 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = [] #storing a tuple
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        for i in range(len(self.cache)):
+            if self.cache[i][0] == key:
+                temp = self.cache.pop(i)
+                self.cache.append(temp)
+                return temp[1]
+            else:
+                continue
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        for i in range(len(self.cache)):
+            if self.cache[i][0] == key:
+                temp = self.cache.pop(i)
+                temp[1] = value
+                self.cache.append(temp)
+                return
+        
+        if self.capacity == len(self.cache):
+            self.cache.pop(0)
+        self.cache.append([key, value])
+            
